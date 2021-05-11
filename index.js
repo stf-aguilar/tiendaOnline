8fe1d8b61c3d4a0c859b0a33d800a571b88767a9
Estefanía <silestagui@gmail.com>
2021-05-11 05:17:13 +0200

    cambio el puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const repository = require('./repository');
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
